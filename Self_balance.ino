aa13cd8a05edc5486cff714dbda8d9fa2e7a3cd3
lokchonmou <lokchonmou@LokdeMacBook-Pro-2.local>
2014-06-27 17:20:27 +0800

    2014 version
    using a 9 dof razor library and a PID library, please download them
    first

--- Self_balance.ino
+++ Self_balance.ino
@@ -1,102 +1,64 @@
-#include <FreeSixIMU.h>
-#include <FIMU_ADXL345.h>
-#include <FIMU_ITG3200.h>
 #include <PID_v1.h>
-#include <Wire.h>
 
-int angles[6]; // yaw pitch roll
-double result[2];
-double Setpoint[2], Input[2], Output[3];
+float yaw, pitch, roll;
 
-//----------------------------------------------------
-double Ka = 0;
-double Kb = 1;
-
-PID myPID(&Input[0], &Output[0], &Setpoint[0],25,0,1, DIRECT);
-PID myPID2(&Input[1], &Output[1], &Setpoint[1],0.02, 0.0005, 0.001, REVERSE);
-//------------------------------------------------
-
-// Set the FreeSixIMU object
-FreeSixIMU sixDOF = FreeSixIMU();
+double Setpoint, Input, Output;
+double Kp, Ki, Kd;
+PID myPID(&Input, &Output, &Setpoint,Kp,Ki,Kd, DIRECT);
 byte E1 = 5;
 byte M1 = 4;
+byte E2 = 6;
+byte M2 = 7;
 
-
-void setup() 
-{ 
-  Setpoint[0] = -10.50;
-  Setpoint[1] = 0;
-
-  Serial.begin(9600);
-  Wire.begin();
-  for (int i = 4; i<8; i++)
-  {
-    pinMode(i, OUTPUT);
-  }
-  delay(5);
-  sixDOF.init(); //begin the IMU
-  delay(5);
-
- myPID.SetOutputLimits(-255, 255);
-  myPID2.SetOutputLimits(-255, 255);
-  myPID.SetMode(MANUAL);
-  myPID2.SetMode(MANUAL);
-
+void setup()
+{
+  Serial.begin(57600);
+  Setpoint = 0;
+  myPID.SetMode(AUTOMATIC);
+  myPID.SetOutputLimits(-255, 255);
+  pinMode(E1, OUTPUT);
+  pinMode(E2, OUTPUT);
+  pinMode(M1, OUTPUT);
+  pinMode(M2, OUTPUT);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
 }
 
-void loop() { 
-  if (millis() >10000  && millis() < 10200) 
+void loop(){
+  if (Serial.available())
   {
-    myPID.SetMode(AUTOMATIC);
-    myPID2.SetMode(AUTOMATIC);
+    yaw = Serial.parseFloat();
+    pitch = Serial.parseFloat();
+    roll = Serial.parseFloat();
   }
-  // sixDOF.getEuler(angles);
 
-  // Serial.print(angles[0]);
-  //Serial.print(" | ");  
-  // Serial.print(angles[1]);
-  // Serial.print(" | ");
-  // Serial.println(angles[2]);
-
-
-
-  // Serial.println(Output); 
-  sixDOF.getRawValues(angles); 
-  result[0] = atan((double)angles[2]/angles[1])*180/3.1415926535897932384626433832795;
-  result[1] = (double)angles[3]*180/3.1415926535897932384626433832795;
-  Input[0] = result[0]; 
-  Input[1] = result[1];
+  //  Serial.print(yaw);
+  //  Serial.print(", ");
+  //  Serial.print(pitch);
+  //  Serial.print(", ");
+  //  Serial.println(roll);
+  
+  Input = pitch;
   myPID.Compute();
-  myPID2.Compute();
-
-   Output[2] = Ka*Output[0] + Kb*Output[1];
-   if (Output[2] >255) Output[2] = 255;
-   else if (Output[2] <-255) Output[2] = -255;
-
-  Serial.print(result[0]);
-  Serial.print(" | ");
-  Serial.print(result[1]);
-  Serial.print(" | ");
-  Serial.println(Output[2]);
-
-/*  if (Output[2]<0)
-   {
-   digitalWrite(M1, HIGH);
-   
-   analogWrite(E1, abs(Output[2]));
-   
-   }
-   else
-   {
-   digitalWrite(M1, LOW);
-   
-   analogWrite(E1, abs(Output[2]));
-   
-   } */
+  //Serial.println(Output);  
+  if (Output > 0)
+  {
+    analogWrite(E1, Output);
+    digitalWrite(M1, HIGH);
+    analogWrite(E2, Output);
+    digitalWrite(M2, HIGH);
+  }
+  else{
+    analogWrite(E1, -Output);
+    digitalWrite(M1, LOW);
+    analogWrite(E2, -Output);
+    digitalWrite(M2, LOW);
+  }
+  Kp = map(analogRead(A0), 0, 1023, 0, 15);
+  Ki = map(analogRead(A1), 0, 1023, 0, 5);
+  Kd = map(analogRead(A2), 0, 1023, 0, 5);
+  myPID.SetTunings(Kp, Ki, Kd);
 }
-
-
-
 
 
 
