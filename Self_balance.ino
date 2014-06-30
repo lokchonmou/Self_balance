c098072fb1e188cdaf82c8bc80c4ca5d43cb769a
lokchonmou <lokchonmou@LokdeMacBook-Pro-2.local>
2014-06-30 12:15:04 +0800

    o
    open wheel version v2

--- Self_balance.ino
+++ Self_balance.ino
@@ -11,8 +11,10 @@ FreeSixIMU sixDOF = FreeSixIMU();
 
 double Kp, Ki, Kd, input, output;
 unsigned long timer, sample_time;
-long cumulative_acc;
-int init_acc, init_gyro, acc_value, gyro_value;
+long cumulative_angle;
+float init_acc, init_gyro, acc_value, gyro_value, angle, gyro_angle;
+double Setpoint, Input, Output;
+PID myPID(&Input, &Output, &Setpoint,Kp,Ki,Kd, DIRECT);
 
 byte E1 = 5;
 byte M1 = 4;
@@ -28,9 +30,11 @@ void setup()
   delay(5);
   
   init_acc=0;
-  init_gyro = 75;
-  sample_time = 200;
-  
+  init_gyro = -6;
+//  sample_time = 200;
+  Setpoint = 0;  
+  myPID.SetMode(AUTOMATIC);
+  myPID.SetOutputLimits(-255, 255);
   pinMode(E1, OUTPUT);
   pinMode(E2, OUTPUT);
   pinMode(M1, OUTPUT);
@@ -45,28 +49,32 @@ void loop(){
   
   acc_value = rawSixDof[1]- init_acc;
   gyro_value = rawSixDof[3] - init_gyro;
-
+  
   if (millis() - timer >= sample_time)   { //do the following every 200ms
-  cumulative_acc += acc_value;
-  output = - (Kp * acc_value + Kd * gyro_value  + Ki * cumulative_acc);
-  output = constrain(output, -255, 255);
+  gyro_angle += gyro_value * sample_time / 1000;
+  gyro_angle = constrain(gyro_angle, -15, 15);
+  angle = 0.8 * acc_value + 0.2 * gyro_angle;
   }
+  
+  Input = angle;
+  myPID.Compute();
 
-  if (output > 0)
+  if (Output > 0)
   {
-    analogWrite(E1, output);
+    analogWrite(E1, Output);
     digitalWrite(M1, HIGH);
-    analogWrite(E2, output);
+    analogWrite(E2, Output);
     digitalWrite(M2, HIGH);
   }
   else{
-    analogWrite(E1, -output);
+    analogWrite(E1, -Output);
     digitalWrite(M1, LOW);
-    analogWrite(E2, -output);
+    analogWrite(E2, -Output);
     digitalWrite(M2, LOW);
   }
   
-  Kp = map(analogRead(A0), 0, 1023, 0, 15);
-  Ki = map(analogRead(A1), 0, 1023, 0, 10);
-  Kd = map(analogRead(A2), 0, 1023, 0, 10);
+  Kp = map(analogRead(A0), 0, 1023, 0, 10);
+  Ki = map(analogRead(A1), 0, 1023, 0, 3);
+  Kd = map(analogRead(A2), 0, 1023, 0, 3);
+  myPID.SetTunings(Kp, Ki, Kd);
 }
