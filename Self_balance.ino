f3e6eb333e0ee68dd6f5154cc8092aa67f7db616
lokchonmou <lokchonmou@LokdeMacBook-Pro-2.local>
2014-06-30 10:39:29 +0800

    Revert 0e16718..54638df

--- Self_balance.ino
+++ Self_balance.ino
@@ -1,19 +1,10 @@
 #include <PID_v1.h>
-#include <FreeSixIMU.h>
-#include <FIMU_ADXL345.h>
-#include <FIMU_ITG3200.h>
-#include <Wire.h>
 
-int rawSixDof[6];
-
-// Set the FreeSixIMU object
-FreeSixIMU sixDOF = FreeSixIMU();
-
-double Kp, Ki, Kd, input, output;
-unsigned long timer, sample_time;
-long cumulative_acc;
-int init_acc, init_gyro, acc_value, gyro_value;
-
+float yaw, pitch, roll;
+const float pi = 3.14159265359;
+double Setpoint, Input, Output;
+double Kp, Ki, Kd;
+PID myPID(&Input, &Output, &Setpoint,Kp,Ki,Kd, DIRECT);
 byte E1 = 5;
 byte M1 = 4;
 byte E2 = 6;
@@ -21,16 +12,10 @@ byte M2 = 7;
 
 void setup()
 {
-  Serial.begin(115200);
-  Wire.begin();
-  delay(5);
-  sixDOF.init(); //begin the IMU
-  delay(5);
-  
-  init_acc=0;
-  init_gyro = 75;
-  sample_time = 200;
-  
+  Serial.begin(57600);
+  Setpoint = 180 * sin (90 - 0);
+  myPID.SetMode(AUTOMATIC);
+  myPID.SetOutputLimits(-255, 255);
   pinMode(E1, OUTPUT);
   pinMode(E2, OUTPUT);
   pinMode(M1, OUTPUT);
@@ -40,33 +25,41 @@ void setup()
 }
 
 void loop(){
- 
-  sixDOF.getRawValues(rawSixDof);
-  
-  acc_value = rawSixDof[1]- init_acc;
-  gyro_value = rawSixDof[3] - init_gyro;
-
-  if (millis() - timer >= sample_time)   { //do the following every 200ms
-  cumulative_acc += acc_value;
-  output = - (Kp * acc_value + Kd * gyro_value  + Ki * cumulative_acc);
-  output = constrain(output, -255, 255);
+  if (Serial.available())
+  {
+    yaw = Serial.parseFloat();
+    pitch = Serial.parseFloat();
+    roll = Serial.parseFloat();
   }
 
-  if (output > 0)
+  //  Serial.print(yaw);
+  //  Serial.print(", ");
+  //  Serial.print(pitch);
+  //  Serial.print(", ");
+  //  Serial.println(roll);
+  
+  Input = 180 * sin (90 - radian(pitch));
+  myPID.Compute();
+  //Serial.println(Output);  
+  if (Output > 0)
   {
-    analogWrite(E1, output);
+    analogWrite(E1, Output);
     digitalWrite(M1, HIGH);
-    analogWrite(E2, output);
+    analogWrite(E2, Output);
     digitalWrite(M2, HIGH);
   }
   else{
-    analogWrite(E1, -output);
+    analogWrite(E1, -Output);
     digitalWrite(M1, LOW);
-    analogWrite(E2, -output);
+    analogWrite(E2, -Output);
     digitalWrite(M2, LOW);
   }
-  
   Kp = map(analogRead(A0), 0, 1023, 0, 15);
-  Ki = map(analogRead(A1), 0, 1023, 0, 10);
-  Kd = map(analogRead(A2), 0, 1023, 0, 10);
+  Ki = map(analogRead(A1), 0, 1023, 0, 5);
+  Kd = map(analogRead(A2), 0, 1023, 0, 5);
+  myPID.SetTunings(Kp, Ki, Kd);
+}
+
+float radian(float degree) {
+  return degree * pi / 180;
 }
